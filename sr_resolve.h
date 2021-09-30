8fcba7e946c109cbbfa170352e781585418a97c4
OkiStuff <probro1802@gmail.com>
2021-09-30 10:30:26 -0400

    modified

--- sr_resolve.h
+++ sr_resolve.h
@@ -6,25 +6,14 @@
 #define SR_RESOLVE_H
 #include <math.h>
 #include <stdio.h>
+#include <Muzzle.h>
 
 #define max(x, y) (x > y ? x : y)
 #define min(x, y) (x > y ? y : x)
 
-typedef struct sr_rec {
-	float x;
-  	float y;
-  	float width;
-  	float height;
-} sr_rec;
-
-typedef struct sr_vec2 {
-	float x;
-	float y;
-} sr_vec2;
-
 typedef struct sr_ray2 {
-    sr_vec2 position;
-    sr_vec2 direction;
+    vec2 position;
+    vec2 direction;
 } sr_ray2;
 
 typedef struct sr_sort_pair {
@@ -39,43 +28,43 @@ void swap_float(float *vec1, float *vec2) {
 }
 
 // Get the length of a vector
-float sr_vec2_length(sr_vec2 v) {
+float sr_vec2_length(vec2 v) {
 	float result = sqrtf((v.x*v.x) + (v.y*v.y));
   	return result;
 }
 
 // Scale a vector by a given float value
-static sr_vec2 sr_vec2_scale(sr_vec2 v, float scale) {
-  	return (sr_vec2){ v.x*scale, v.y*scale };
+static vec2 sr_vec2_scale(vec2 v, float scale) {
+  	return (vec2){ v.x*scale, v.y*scale };
 }
 
 // Divide two vectors
-static sr_vec2 sr_vec2_divide(sr_vec2 v1, sr_vec2 v2) {
-  	return (sr_vec2){ v1.x/v2.x, v1.y/v2.y };
+static vec2 sr_vec2_divide(vec2 v1, vec2 v2) {
+  	return (vec2){ v1.x/v2.x, v1.y/v2.y };
 }
 
 // Multiply two vectors
-static sr_vec2 sr_vec2_multiply(sr_vec2 v1, sr_vec2 v2) {
-  	return (sr_vec2){ v1.x*v2.x, v1.y*v2.y };
+static vec2 sr_vec2_multiply(vec2 v1, vec2 v2) {
+  	return (vec2){ v1.x*v2.x, v1.y*v2.y };
 }
 
 // Normalize a vector
-static sr_vec2 sr_vec2_normalize(sr_vec2 v) {
+static vec2 sr_vec2_normalize(vec2 v) {
   	return sr_vec2_scale(v, 1 / sr_vec2_length(v));
 }
 
 // Substract two vectors
-static sr_vec2 sr_vector2_sub(sr_vec2 v1, sr_vec2 v2) {
-  	return (sr_vec2){ v1.x - v2.x, v1.y - v2.y };
+static vec2 sr_vector2_sub(vec2 v1, vec2 v2) {
+  	return (vec2){ v1.x - v2.x, v1.y - v2.y };
 }
 
 // Add two vectors
-static sr_vec2 sr_vector2_add(sr_vec2 v1, sr_vec2 v2) {
-  	return (sr_vec2){ v1.x + v2.x, v1.y + v2.y };
+static vec2 sr_vector2_add(vec2 v1, vec2 v2) {
+  	return (vec2){ v1.x + v2.x, v1.y + v2.y };
 }
 
 // Check collision between two rectangles
-static bool sr_check_rec_vs_rec_collision(sr_rec rec1, sr_rec rec2) {
+static bool sr_check_rec_vs_rec_collision(rectangle rec1, rectangle rec2) {
     if (
 		(rec1.x < (rec2.x + rec2.width) && (rec1.x + rec1.width) > rec2.x) &&
         (rec1.y < (rec2.y + rec2.height) && (rec1.y + rec1.height) > rec2.y)
@@ -87,18 +76,18 @@ static bool sr_check_rec_vs_rec_collision(sr_rec rec1, sr_rec rec2) {
 }
 
 // Check collision between a ray and a rectangle
-static bool sr_check_ray_vs_rec_collision(const sr_ray2 ray, const sr_rec target, sr_vec2 *contact_point, sr_vec2 *contact_normal, float *t_hit_near) {
+static bool sr_check_ray_vs_rec_collision(const sr_ray2 ray, const rectangle target, vec2 *contact_point, vec2 *contact_normal, float *t_hit_near) {
     // Cache division
-    sr_vec2 indiv = sr_vec2_divide((sr_vec2){1.0f, 1.0f}, ray.direction);
+    vec2 indiv = sr_vec2_divide((vec2){1.0f, 1.0f}, ray.direction);
 
     // Calculate intersections with rectangle bounding axes
-    sr_vec2 t_near = sr_vec2_multiply(
-        sr_vector2_sub((sr_vec2){ target.x, target.y }, ray.position),
+    vec2 t_near = sr_vec2_multiply(
+        sr_vector2_sub((vec2){ target.x, target.y }, ray.position),
         indiv
     );
-    sr_vec2 t_far = sr_vec2_multiply(
+    vec2 t_far = sr_vec2_multiply(
         sr_vector2_sub(
-            sr_vector2_add((sr_vec2){target.x, target.y}, (sr_vec2){target.width, target.height}),
+            sr_vector2_add((vec2){target.x, target.y}, (vec2){target.width, target.height}),
             ray.position
         ),
         indiv
@@ -129,24 +118,24 @@ static bool sr_check_ray_vs_rec_collision(const sr_ray2 ray, const sr_rec target
 
 	if (t_near.x > t_near.y)
 		if (ray.direction.x < 0)
-			*contact_normal = (sr_vec2){ 1, 0 };
+			*contact_normal = (vec2){ 1, 0 };
 		else
-			*contact_normal = (sr_vec2){ -1, 0 };
+			*contact_normal = (vec2){ -1, 0 };
 	else if (t_near.x < t_near.y)
 		if (ray.direction.y < 0)
-			*contact_normal = (sr_vec2){ 0, 1 };
+			*contact_normal = (vec2){ 0, 1 };
 		else
-			*contact_normal = (sr_vec2){ 0, -1 };
+			*contact_normal = (vec2){ 0, -1 };
 
     return true;
 }
 
-static bool sr_dynamic_rect_vs_rect(const sr_rec in, const sr_rec target, sr_vec2 vel, sr_vec2 *contact_point, sr_vec2 *contact_normal, float *contact_time, float delta) {
+static bool sr_dynamic_rect_vs_rect(const rectangle in, const rectangle target, vec2 vel, vec2 *contact_point, vec2 *contact_normal, float *contact_time, float delta) {
     // Check if dynamic rectangle is actually moving - we assume rectangles are NOT in collision to start
     if (vel.x == 0 && vel.y == 0) return false;
     
 	// Expand target rectangle by source dimensions
-    sr_rec expanded_target;
+    rectangle expanded_target;
     expanded_target.x = target.x - (in.width/2);
     expanded_target.y = target.y - (in.height/2);
     expanded_target.width = target.width + in.width;
@@ -154,7 +143,7 @@ static bool sr_dynamic_rect_vs_rect(const sr_rec in, const sr_rec target, sr_vec
 
     if (sr_check_ray_vs_rec_collision(
       (sr_ray2){
-        (sr_vec2){
+        (vec2){
           in.x + (in.width/2),
           in.y + (in.height/2)
         },
@@ -199,12 +188,12 @@ void sr_sort_indexes(sr_sort_pair *times, int length) {
     // }
 }
 
-static void sr_move_and_slide(sr_rec *obstacles, int obstacles_length, sr_vec2 hitbox, sr_vec2 *vel, sr_vec2 *pos , float delta) {
+static void sr_move_and_slide(rectangle *obstacles, const int obstacles_length, vec2 hitbox, vec2 *vel, vec2 *pos , float delta) {
 	sr_sort_pair times[obstacles_length];
-	sr_vec2 cp, cn;
+	vec2 cp, cn;
 	float time = 0;
 
-  	sr_rec hitbox_rec = {
+  	rectangle hitbox_rec = {
   	  	pos->x - (hitbox.x/2),
   	  	pos->y - (hitbox.y/2),
   	  	hitbox.x,
